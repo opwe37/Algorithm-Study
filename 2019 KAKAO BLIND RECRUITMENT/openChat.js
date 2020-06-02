1f5637d4d4ecba7297069200e75e7470d9d0181d
opwe37 <opwe37@naver.com>
2020-06-02 16:39:05 +0900

    Update openChat.js

--- 2019 KAKAO BLIND RECRUITMENT/openChat.js
+++ 2019 KAKAO BLIND RECRUITMENT/openChat.js
@@ -2,21 +2,19 @@ function solution(record) {
   var answer = [];
 
   var records = new Map();
-  for (let i = 0; i < record.length; i++) {
-    let tmp = record[i].split(' ');
-    if (tmp[0] == 'Enter' || tmp[0] == 'Change') {
-        records.set(tmp[1], tmp[2]);
-    }
-  }
 
-  for (let i = 0; i < record.length; i++) {
-    let tmp = record[i].split(' ');
-    if (tmp[0] == 'Enter') {
-      answer.push(records.get(tmp[1]) + '님이 들어왔습니다.');
-    } else if (tmp[0] == 'Leave') {
-      answer.push(records.get(tmp[1]) + '님이 나갔습니다.');
+  record.forEach(function(e){
+    const [type, uid, name] = e.split(' ');
+    if (type == 'Enter' || type == 'Change') {
+        records.set(uid, name);
     }
-  }
+  })
+
+  record.forEach(function(e) {
+    const [type, uid] = e.split(' ');
+    if (type == 'Enter') answer.push(records.get(uid) + '님이 들어왔습니다.');
+    if (type == 'Leave') answer.push(records.get(uid) + '님이 나갔습니다.');
+  })
 
   return answer;
 }

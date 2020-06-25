e5bcd0a72cf165ffaad85a4fe813806117adb8cd
opwe37 <opwe37@naver.com>
2020-06-25 16:26:54 +0900

    solution : Adjacency List & BFS

--- DFS&BFS/LongestNode.js
+++ DFS&BFS/LongestNode.js
@@ -1,9 +1,10 @@
+// Approach. Adjacency List & BFS
 function solution(n, edge) {
   var answer = 0;
-  var edges = Array(n).fill(0).map(x => new Array(n).fill(0));
-  for (let [row, column] of edge) {
-    edges[row-1][column-1] = 1;
-    edges[column-1][row-1] = 1;
+  var edges = Array(n).fill(0).map(val => new Array);
+  for (let [row, col] of edge) {
+    edges[row-1].push(col-1);
+    edges[col-1].push(row-1);
   }
 
   var isVisit = new Array(n).fill(false);
@@ -14,8 +15,8 @@ function solution(n, edge) {
     let loop = possibleNode.length;
     for (let i = 0; i < loop; i++) {
       let node = possibleNode.shift();
-      for (let col = 0; col < n; col++) {
-        if (edges[node][col] == 0 || isVisit[col]) continue;
+      for (let col of edges[node]) {
+        if (isVisit[col]) continue;
 
         possibleNode.push(col);
         isVisit[col] = true;
@@ -26,3 +27,33 @@ function solution(n, edge) {
 
   return answer;
 }
+
+// Approach. Adjacency Matrix & BFS
+// function solution(n, edge) {
+//   var answer = 0;
+//   var edges = Array(n).fill(0).map(x => new Array(n).fill(0));
+//   for (let [row, column] of edge) {
+//     edges[row-1][column-1] = 1;
+//     edges[column-1][row-1] = 1;
+//   }
+
+//   var isVisit = new Array(n).fill(false);
+//   var possibleNode = [0];
+//   isVisit[0] = true;
+  
+//   while (possibleNode.length != 0) {
+//     let loop = possibleNode.length;
+//     for (let i = 0; i < loop; i++) {
+//       let node = possibleNode.shift();
+//       for (let col = 0; col < n; col++) {
+//         if (edges[node][col] == 0 || isVisit[col]) continue;
+
+//         possibleNode.push(col);
+//         isVisit[col] = true;
+//       }
+//     }
+//     answer = loop;
+//   }
+
+//   return answer;
+// }

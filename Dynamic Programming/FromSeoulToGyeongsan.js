eb2d32647f3f65624db454d075ec7b9c8536b0d0
opwe37 <opwe37@naver.com>
2020-07-15 14:51:37 +0900

    Create FromSeoulToGyeongsan.js

---
+++ Dynamic Programming/FromSeoulToGyeongsan.js
@@ -0,0 +1,29 @@
+function solution(K, travel) {
+    var answer = 0;
+    
+    const n = travel.length;
+    
+    var dp = new Array(travel.length).fill(0).map(val => new Array(K+1).fill(0));
+    dp[0][travel[0][0]] = travel[0][1];
+    dp[0][travel[0][2]] = travel[0][3];
+
+    for (let i = 1; i < n; i++) {
+        for (let time = 0; time < K+1; time++) {
+            let walk_time = travel[i][0]
+              , walk_value = travel[i][1]
+              , bycicle_time = travel[i][2]
+              , bycicle_value = travel[i][3];
+
+            if ((time - walk_time) >= 0 && dp[i-1][time - walk_time] != 0) {
+                let val = dp[i-1][time - walk_time] + walk_value;
+                dp[i][time] = val > dp[i][time] ? val : dp[i][time];
+            }
+            if ((time - bycicle_time) >= 0 && dp[i-1][time - bycicle_time] != 0){
+                let val = dp[i-1][time - bycicle_time] + bycicle_value;
+                dp[i][time] = val > dp[i][time] ? val : dp[i][time];
+            }
+        }
+    }
+
+    return answer = Math.max.apply(null, dp[travel.length-1]);
+}

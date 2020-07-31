e94879354fd29dfc02bf895baa7c840eb03b186f
opwe37 <opwe37@naver.com>
2020-07-31 17:30:36 +0900

    Update BuyCookie.js

--- General/BuyCookie.js
+++ General/BuyCookie.js
@@ -44,23 +44,17 @@ function solution2(cookie) {
         let r_val = sum[r_idx] - sum[m];
 
         while (true) {
-            console.log(l_val, l_idx, r_val, r_idx, m);
             if (l_val == r_val && answer < l_val) {
                 answer = l_val;
                 break;
             }
 
-            if (l_val < r_val && r_idx > m+1){
-                r_idx--;
-                r_val = sum[r_idx] - sum[m];
-            } else if (r_val < l_val && l_idx < m) {
-                l_idx++;
-                l_val = sum[m] - sum[l_idx-1];
-            } else {
-                break;
-            }
+            if (l_val < r_val && r_idx > m+1)    r_val -= cookie[r_idx--];
+            else if (r_val < l_val && l_idx < m)    l_val -= cookie[l_idx++];
+            else    break;
         }
     }
 
     return answer;
 }
+

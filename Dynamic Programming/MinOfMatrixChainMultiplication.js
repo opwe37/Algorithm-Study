5ceacfb0af606e57fe94619fcc66c8245d40a260
opwe37 <opwe37@naver.com>
2020-08-03 14:53:12 +0900

    Update MinOfMatrixChainMultiplication.js

--- Dynamic Programming/MinOfMatrixChainMultiplication.js
+++ Dynamic Programming/MinOfMatrixChainMultiplication.js
@@ -3,15 +3,15 @@ function solution(matrix_size) {
 
     const n = matrix_sixe.length;
     var m = new Array(n+1).fill().map(() => new Array(n+1).fill(0));
-    var d = [];
+    var dims = [];
     for (let i in matrix_size) {
         if (i == 0) {
-            d.push(matrix_size[i][0]);
-            d.push(matrix_size[i][1]);
+            dims.push(matrix_size[i][0]);
+            dims.push(matrix_size[i][1]);
             continue;
         }
 
-        d.push(matrix_size[i][1]);
+        dims.push(matrix_size[i][1]);
     }
     
     for (let len = 2; len < n+1; len++) {
@@ -19,7 +19,7 @@ function solution(matrix_size) {
             let j = i + len -1;
             m[i][j] = Infinity;
             for (let k = i; k < j; k++) {
-                m[i][j] = Math.min(m[i][j], m[i][k]+m[k+1][j]+(d[i-1]*d[k]*d[j]));
+                m[i][j] = Math.min(m[i][j], m[i][k]+m[k+1][j]+(dims[i-1]*dims[k]*dims[j]));
             }
         }
     }

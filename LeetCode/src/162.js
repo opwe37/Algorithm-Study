c4251049fbcaa77e6224d159c61d8b6fa7e36318
opwe37 <opwe37@naver.com>
2021-06-14 12:37:47 +0900

    Find Peak Element

---
+++ LeetCode/src/162.js
@@ -0,0 +1,15 @@
+var findPeakElement = function(nums) {
+    let left = 0, right =nums.length-1;
+    
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        
+        if (nums[mid] < nums[mid+1]) {
+            left = mid + 1;
+        } else{
+            right = mid;
+        }
+    }
+    
+    return left;
+};

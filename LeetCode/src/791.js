21670133dbc0a92ea2f4852454d17cce248a94c3
opwe37 <opwe37@naver.com>
2021-07-15 09:12:37 +0900

    2021.07.15 _ Custom Sort String _ add new solution

--- LeetCode/src/791.js
+++ LeetCode/src/791.js
@@ -1,25 +1,49 @@
-var customSortString = function(S, T) {
-    const dict = new Map();
-    S.split('').forEach((char, idx) => {
-        dict.set(char, idx);
-    });
+// ==============================================================================
+// O(nlogn) Sort Algorithm
+
+var customSortString = function(order, str) {
+    const customOrder = new Array(26).fill(27);
     
-    let T_arr = T.split('');
-    T_arr = T_arr.sort((a, b) => {
-        if (dict.has(a) && dict.has(b)) {
-            return dict.get(a) - dict.get(b);
-        }
+    const codeA = 'a'.charCodeAt(0);
+    for (let i = 0; i < order.length; i++) {
+        const ascii = order[i].charCodeAt(0)
         
-        if (dict.has(a)) {
-            return -1;
-        }
-        
-        if (dict.has(b)) {
-            return 1;
-        }
-        
-        return 0;
-    });
+        customOrder[ascii - codeA] = i + 1;
+    }
     
-    return T_arr.join('');
+    const strList = str.split('');
+    const ansList = strList.sort((a, b) => {
+        return customOrder[a.charCodeAt(0) - codeA] - customOrder[b.charCodeAt(0) - codeA]
+    })
+    
+    return ansList.join('');
+};
+
+
+// ==============================================================================
+// O(n + k) Counting Sort
+
+var customSortString = function(order, str) {
+    let ans = '';
+    
+    const charMap = new Map();
+    for (let i = 0; i < str.length; i++) {
+        if (!charMap.has(str[i])) { charMap.set(str[i], 0); }
+        charMap.set(str[i], charMap.get(str[i]) + 1);
+    }
+    
+    // order에 정의된 문자
+    for (let i = 0; i < order.length; i++) {
+        if (!charMap.has(order[i])) { continue; }
+        
+        ans += order[i].repeat(charMap.get(order[i]));
+        charMap.delete(order[i]);
+    }
+    
+    // order에 정의되지 않은 나머지 문자
+    for (let [char, freq] of charMap.entries()) {
+        ans += char.repeat(freq);
+    }
+    
+    return ans;
 };

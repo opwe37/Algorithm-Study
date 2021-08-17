7159cd7c98b328203b32908755f32a52ada4dab8
opwe37 <opwe37@naver.com>
2021-08-17 18:12:03 +0900

    2021.08.21 _ Range Sum Query - Immutable
    [Update] Prefix Sum Approach

--- LeetCode/src/303.js
+++ LeetCode/src/303.js
@@ -7,3 +7,18 @@ NumArray.prototype.sumRange = function(left, right) {
     for (let i = left; i <= right; i++) { answer += this.nums[i]; }
     return answer;
 };
+
+// =============================================================================
+// Prefix Sum
+var NumArray_prefixSum = function(nums) {
+    this.prefix = [nums[0]];
+    
+    for (let i = 1; i < nums.length; i++) {
+        this.prefix[i]  = nums[i] + this.prefix[i-1];
+    }
+};
+
+NumArray_prefixSum.prototype.sumRange = function(left, right) {
+    return this.prefix[right] - (left-1 < 0 ? 0 : this.prefix[left-1]);
+};
+

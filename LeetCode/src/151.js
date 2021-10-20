18eb80ebed76a21d52dfca3d0be5c2ea90ab9779
opwe37 <opwe37@gmail.com>
2021-10-20 17:20:45 +0900

    2021.10.20 _ Reverse Words in a String

--- LeetCode/src/151.js
+++ LeetCode/src/151.js
@@ -1,22 +1,29 @@
 var reverseWords = function(s) {
+    let answer = '';
+    
+    const words = splitWords(s);
+    while (words.length) {
+    	answer += words.pop() + (words.length ? ' ' : '');
+    }
+    
+    return answer;
+}
+
+function splitWords(s) {
     const words = [];
+    
     let word = '';
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === ' ') {
-            if (word) { 
-                words.push(word);
-                word = '';
+    	if (s[i] === ' ') {
+            if (word) {
+            	words.push(word);
+            	word = '';
             }
             continue;
         }
-        
         word += s[i];
     }
-    if (word) { words.push(word) }
+    if (word) { words.push(word); }
     
-    let answer = '';
-    while (words.length) {
-        answer += words.pop() + (words.length ? ' ' : '');
-    }
-    return answer;
-};
+    return words;
+}
